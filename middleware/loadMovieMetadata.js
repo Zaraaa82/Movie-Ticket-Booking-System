e0c75709c5b8d8a172a306400063bb072ba3c06d
Zahraa Alaiwi <zozoa.jalil@gmail.com>
2026-07-16 21:02:46 +0300

    feat: add movie metadata middleware for forms

---
+++ middleware/loadMovieMetadata.js
@@ -0,0 +1,35 @@
+const Genre = require("../models/Genre");
+
+async function movieMetadata(req, res, next) {
+    try{
+        res.locals.genres = (await Genre.find()).map(genre => genre.name);
+        res.locals.languages = [
+            "English",
+            "Arabic",
+            "French",
+            "Spanish",
+            "German",
+            "Italian",
+            "Japanese",
+            "Korean",
+            "Hindi",
+            "Mandarin",
+            "Russian",
+            "Turkish"
+        ];
+        res.locals.ageRatings  = [
+            "G",
+            "PG",
+            "PG-13", 
+            "R", 
+            "NC-17"
+        ];
+        next();
+    }catch(error){
+        console.log(error);
+        res.send("Something went wrong.");
+    }
+
+};
+
+module.exports = movieMetadata;
