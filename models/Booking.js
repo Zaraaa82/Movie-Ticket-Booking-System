1454b616f2b72eaa0d67bb60af293d4aaca61ce9
Zahraa Alaiwi <zozoa.jalil@gmail.com>
2026-07-23 01:24:36 +0300

    refactor: remove snack schema from booking model

--- models/Booking.js
+++ models/Booking.js
@@ -1,18 +1,6 @@
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const snackSchema = new mongoose.Schema({
-  snack: {
-    type: ObjectId,
-    ref: "Snack",
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    min: 0,
-    required: true
-  },
-});
 
 const bookingSchema = new mongoose.Schema(
   {
